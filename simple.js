b46fe679e9e83b1ec81bdfecbaaaf95e0a5d642a
Dreklorian <sarkos381@yahoo.com>
2023-10-21 08:48:56 -0700

    more test lines

--- simple.js
+++ simple.js
@@ -1,3 +1,22 @@
 console.log(9.3 % 3.1);
 console.log(9 % 3.1);
 console.log(5/2.1111111111);
+let a = "5";
+let b = 5;
+let c = '5';
+console.log(a == b);
+console.log(b !== c);
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(!!!false);
+console.log(a + c);
+let test = "This is a TEST.";
+test[1] = test[1].toUpperCase()
+console.log(test);
+console.log(typeof(test[1]));
+const newtest = 4
+console.log(newtest);
+let dog;
+console.log(dog + "god");
+newtest + 4;
